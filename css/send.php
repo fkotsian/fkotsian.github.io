3daca9b1420270029cdf9ded679ce01c28bef379
App Academy Student <student@appacademy.io>
2014-06-03 21:09:04 -0700

    Implement contact form email

--- css/send.php
+++ css/send.php
@@ -1,13 +1,15 @@
-$name = $_POST['name'];
-$email = $_POST['email'];
+$name    = $_POST['name'];
+$email   = $_POST['email'];
 $message = $_POST['message'];
  
-$to = 'frankkotsianas@gmail.com';
-$subject = 'Contact from ' + $name;
-$message = 'FROM: '.$name.' Email: '.$email.'Message: '.$message;
-$headers = 'From: contact@frankkotsianas.com';
+$to      = 'frankkotsianas@gmail.com';
+$subject = 'Contact from '.$name';
+$message = 'FROM: '.$name.' Email: '.$email.Message: '.$message;
+$headers = 'From: contact@frankkotsianas.com' . "\r\n";
  
 if (filter_var($email, FILTER_VALIDATE_EMAIL)) { 
-mail($to, $subject, $message) or die('Error sending Mail'); 
-echo "Your email was sent!";
+  mail($to, $subject, $message, $headers) or die('Error sending Mail'); 
+  echo "Your email was sent!";
+} else {
+  echo "Invalid email! Please provide a valid email address.";
 }
